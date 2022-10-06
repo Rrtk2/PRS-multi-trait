66a81d2e2ecb804acd0b90cffebee1e210d389fd
Rick <Rick.Reijnders@home.nl>
2022-10-06 09:15:02 +0200

    updated outdated paths

--- Scripts/LDAK/LDAK.R
+++ Scripts/LDAK/LDAK.R
@@ -23,8 +23,8 @@
 #-----------------------------------------------------------------------------------------------------#
 source("C:/DATA_STORAGE/Projects/PRS-multi-trait/Scripts/.Main/Settings.R")
 
-ldak = paste0("/mnt/c/DATA_STORAGE/Projects/PRS/Programs/ldak5.2.linux_/ldak5.2.linux")
-data_loc = paste0("/mnt/c/DATA_STORAGE/Projects/PRS/Data_RAW/Test_dataset/data/")
+ldak = paste0("/mnt/c/DATA_STORAGE/Projects/PRS-multi-trait/Programs/ldak5.2.linux_/ldak5.2.linux")
+data_loc = paste0("/mnt/c/DATA_STORAGE/Projects/PRS-multi-trait/Data_RAW/Test_dataset/data/")
 
 
 #-----------------------------------------------------------------------------------------------------#
