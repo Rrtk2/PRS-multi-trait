5c7db75ee5afc3f94c22fc33aaf182f6a2636016
Reijnders <P70072451@unimaas.nl>
2023-11-10 11:36:21 +0100

    Added a additional step that filters the data to the SNPs used, and remove misaligned SNPS.

--- Package/PRSMultiTrait/R/checkDataIDs.R
+++ Package/PRSMultiTrait/R/checkDataIDs.R
@@ -19,7 +19,7 @@ checkDataIDs = function(path = NA){
 	message("(checkDataIDs.R)    Checking IDs on '",path,"'!")
 	
 	#-----------------------------------------------------------------------------------------------------#
-	#							MAIN
+	#							Check IDs
 	#-----------------------------------------------------------------------------------------------------#
 
 	# load the bimfile 
@@ -30,14 +30,12 @@ checkDataIDs = function(path = NA){
 	check_out = as.logical(table((grepl(head(bim$V2,check_number_snps),pattern = ":") & 
 	length(strsplit(head(bim$V2,check_number_snps),":")) == 1))["TRUE"] == check_number_snps) & !(FALSE%in%(lapply(strsplit(head(bim$V2,check_number_snps),":"),length)==2))
 
-	
-
-	
+		
 	if(check_out){
-		cat("ID is ok!\n\n")
+		cat("(checkDataIDs.R)    ID is ok!\n\n")
 		
 	}else{
-		cat("ID is transformed to chr:bp!\n\n")
+		cat("(checkDataIDs.R)    ID is transformed to chr:bp!\n\n")
 		
 		CHR.BP = paste(bim$V1,bim$V4,sep = ":")
 		SNPconv = data.frame(bim$V2,CHR.BP)
@@ -58,5 +56,58 @@ checkDataIDs = function(path = NA){
 
 		message("(checkDataIDs.R)    New bfiles generated with name: ",paste0(gsub(path_data_wsl ,pattern = "^.*/",replacement = ""),"_chrbp_nodup"),"!")
 	}
+
+
+
+	#-----------------------------------------------------------------------------------------------------#
+	#							Check misalighned SNPs
+	#-----------------------------------------------------------------------------------------------------#
+	# Check if the Data has any misalighned SNPs or such, check with HAPMAP, the database we use to set up the PGS models. If these align here, it should not throw errors later in the generation of PGS
+	# In the past this has happend, that the data (cohort) had mismatched SNPs, throwing error.
+	cat("(checkDataIDs.R)    Checking misaligned SNPs\n\n")
+	
+	# check if there are any that are misaligned to the referecne (UK biobank)
+	load(paste0(Settings_env$s_ROOT_dir,"Reference/gbr_hapmap/All_SNPS_Hapmap.Rdata")) # waaay faster now!
+	temp_gwas_dim_pre = length(CHR.BP )
+	#temp_gwas = temp_gwas[CHR.BP%in%All_SNPS_Hapmap,]
+	SNPselectList = CHR.BP%in%All_SNPS_Hapmap
+	temp_gwas_dim_post = sum(SNPselectList)
+	cat("(checkDataIDs.R)    ",(temp_gwas_dim_post[1]), "(",paste0(round(((temp_gwas_dim_post[1]) / temp_gwas_dim_pre[1])*100,1),"% )"," SNPs remaining after Hapmap filter"),"\n")
+	
+	# write the SNPs to be selected
+	data.table::fwrite(data.frame(CHR.BP[SNPselectList]), file = paste0(path_SNPnames,"SNPselect.txt"), quote = FALSE, col.names = FALSE, row.names = FALSE,sep="\t")
+
+	# ID potential duplcated IDs
+	#plink --file input_file --list-duplicate-vars
+	system(paste(Settings_env$s_plinkloc,"--bfile",paste0(path_data_wsl,"_chrbp_nodup")," --extract ",paste0(path_SNPnames,"SNPselect.txt")," --make-bed --out",paste0(path_data_wsl,"_chrbp_nodup_hapmapfilter")))
+	
+	# Check if Alleles are correct (MAF should be Minor, not Major)
+	# get the reference HAPMAP
+	temp_ref = data.table::fread(paste0(Settings_env$s_ROOT_dir,"Reference/gbr_hapmap/gbr.hapmap.cors.bim"),header = FALSE,sep = "\t",fill=TRUE) # waaay faster now!
+	
+	# get the newly made fixed data 
+	temp_data = data.table::fread(paste0(path_data_wsl,"_chrbp_nodup_hapmapfilter.bim"),header = FALSE,sep = "\t",fill=TRUE) # waaay faster now!
+	
+		
+	# Joining on relevant columns (V2 in this case)
+	merged_data = merge(temp_data, temp_ref, by = "V2", suffixes = c("_temp_data", "_temp_ref"))
+
+	# Checking if V5 and V6 align for all SNPs; and also case where all is flipped
+	temp_align = merged_data$V5_temp_data == merged_data$V5_temp_ref & merged_data$V6_temp_data == merged_data$V6_temp_ref | merged_data$V5_temp_data == merged_data$V6_temp_ref & merged_data$V6_temp_data == merged_data$V5_temp_ref
+
+	cat("   of which",sum(!temp_align),"SNPs were misaligned (removed)","\n")
+		
+
+	# write the SNPs to be selected
+	data.table::fwrite(data.frame(merged_data$V2[temp_align]), file = paste0(path_SNPnames,"SNPselect.txt"), quote = FALSE, col.names = FALSE, row.names = FALSE,sep="\t")
+
+	# ID potential duplcated IDs
+	#plink --file input_file --list-duplicate-vars
+	system(paste(Settings_env$s_plinkloc,"--bfile",paste0(path_data_wsl,"_chrbp_nodup_hapmapfilter")," --extract ",paste0(path_SNPnames,"SNPselect.txt")," --make-bed --out",paste0(path_data_wsl,"_chrbp_nodup_hapmapfilter_done")))
+	
+
+	#cat("   of which",(as.numeric(table(temp_A12A21_index)["TRUE"])), "(",paste0(round(as.numeric(table(temp_A12A21_index)["TRUE"] / temp_gwas_dim_pre_init[1])*100,1),"% )"," SNPs were flipped"),"\n")
+	cat("(checkDataIDs.R)    ",(length(merged_data$V2[temp_align])), "(",paste0(round(((length(merged_data$V2[temp_align])) / temp_gwas_dim_pre[1])*100,1),"% )"," SNPs remaining after Hapmap filter"),"\n")
+	message("(checkDataIDs.R)    New bfiles generated with name: ",paste0(gsub(path_data_wsl ,pattern = "^.*/",replacement = ""),"_chrbp_nodup_hapmapfilter_done"),"!")
 
 }
